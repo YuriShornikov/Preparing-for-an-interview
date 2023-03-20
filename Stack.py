fc440c7ebaeb0b685ca7987e7d56f78c26435ce1
Yuri Shornikov <dbonlyfun@gmail.com>
2023-03-20 14:33:25 +0300

    2

--- Stack.py
+++ Stack.py
@@ -28,29 +28,22 @@ class Stack:
     def size(self):
         return len(self.stack)
 
-# stack = ['(','(','(','(','[','{','}',']',')',')',')',')']
-# # print(stack[1])
-
-
-if __name__ == '__main__':
-    S = Stack()
-
 
 #№2
 check_list_1 = ['(','(','(','(','[','{','}',']',')',')',')',')']
 check_list_2 = ['[','(','[',']',')','(','(','(','[','[','[',']',']',']',')',')',')',']','{','(',')','}']
 check_list_3 = ['{','{','[','(',')',']','}','}']
-check_lists_balance = [check_list_1, check_list_2, check_list_3]
 
 check_list_no_1 = ['}','{',')']
 check_list_no_2 = ['{','{','[','(',']',')',']','}','}']
 check_list_no_3 = ['[','[','{','(',')',')','}',']']
 
+check_lists_balance = [check_list_1, check_list_2, check_list_3, check_list_no_1, check_list_no_2, check_list_no_3]
+
 check_dict = {'(': ')', '[': ']', '{': '}'}
 
 
 def check_list(check_list):
-    num = 0
     if len(check_list) % 2 == 1:
         return f'Несбалансированно'
     else:
@@ -59,20 +52,23 @@ def check_list(check_list):
                 S.push(symbol)
                 print(S.stack)
             else:
-                S.pop()
-                print(S.stack)
-            num += 1
+                last_symbol = S.peek()
+                if check_dict.get(last_symbol) == symbol:
+                    S.pop()
+                    print(S.stack)
+                else:
+                    return f'Несбалансированно'
+
         S.is_empty()
         return f'Cбалансированно'
 
 #Проверка на Сбалансированность
-for num in range(len(check_lists_balance)):
-    check = check_list(check_lists_balance[num])
-    print(check)
+if __name__ == '__main__':
+    S = Stack()
+    for num in range(len(check_lists_balance)):
+        check = check_list(check_lists_balance[num])
+        print(check)
+        print()
 
-
-
-no = check_list(check_list_no_3)
-print(no)
 
 
